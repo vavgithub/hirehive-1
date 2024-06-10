3fb3a66437db1be2d45b2dbb38313ee1de785276
vevaar <vevaaratvav@gmail.com>
2024-06-10 15:06:15 +0530

    fixes done

--- client/src/pages/Admin/ViewJobs.jsx
+++ client/src/pages/Admin/ViewJobs.jsx
@@ -168,41 +168,9 @@ const ViewJobs = () => {
 
     const currentPage = 'viewJob';
 
-    const columnsData = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 90,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-        },
-    ];
-
-    const rowsData = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
-
 
     const candidateStats = [
-        { title: 'Total', value: `${rowsData.length}` },
+        { title: 'Total', value: "0" },
         { title: 'Portfolio', value: '0' },
         { title: 'Screening', value: '0' },
         { title: 'Design Task', value: '0' },
@@ -213,7 +181,7 @@ const ViewJobs = () => {
     ];
 
     const jobsDetailStats = [
-        { title: 'Views', value: `${rowsData.length}` },
+        { title: 'Views', value: "0" },
         { title: 'Applications Received', value: '156' },
         { title: 'Qualified applications', value: '80' },
         { title: 'Engagement Rate', value: '78%' },
@@ -273,7 +241,7 @@ const ViewJobs = () => {
                     <div>
                         <StatsGrid stats={candidateStats} />
                         <div style={{ height: 400, width: '100%' }}>
-                            <DataTable rows={rowsData} columns={columnsData} />
+                            <DataTable/>
                         </div>
                     </div>
 
