9dd37dc0512cfeedb6a91ddecbc5309e77ac020e
vevaar <vevaaratvav@gmail.com>
2024-09-01 12:04:30 +0530

    same site : none

--- server/controllers/admin/auth.controller.js
+++ server/controllers/admin/auth.controller.js
@@ -3,8 +3,9 @@ import {User} from '../../models/admin/user.model.js';
 import generateToken from '../../utils/generateToken.js';
 
 const cookieOptions = {
-  httpOnly: true, 
+  httpOnly: true,
   secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+  sameSite:"none", 
   maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
 };
 
