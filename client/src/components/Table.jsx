f8501d3ccab2b83cee76287f550695b8ebb64fdf
kiranatvav <kiranatvav@gmail.com>
2025-01-31 17:19:02 +0530

    rating disable on rejected copied

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -621,7 +621,7 @@ const Table = ({ jobId, readOnly = false, readOnlyData = [] }) => {
           >
             {canReject(params.row) ? <RejectActive /> : <Reject />}
           </button>
-          <button onClick={(e) => handleRatingClick(e, params.row)}>
+          <button className={params?.row?.stageStatuses[params?.row?.currentStage]?.status === "Rejected" ? "text-font-gray" : "text-white"} disabled={params?.row?.stageStatuses[params?.row?.currentStage]?.status === "Rejected"} onClick={(e) => handleRatingClick(e, params.row)}>
             {getRatingIcon(params.row.rating)}
           </button>
         </div>
