e5e929169a80c38331db3301a11bec20cb833e7f
vavgithub <helloatvavgithub.com>
2025-02-17 19:05:01 +0530

    edit cpp is sorted

--- client/src/utility/validationRules.js
+++ client/src/utility/validationRules.js
@@ -69,12 +69,11 @@ export const validationRules = {
       message: "Expected CTC must be a two-digit value (e.g., 3 or 15)",
     },
     // Custom validation to ensure expectedCTC is greater than currentCTC
-    validate: (value, formValues) => {
-      const currentCTC = parseFloat(formValues.currentCTC);
-      return (
-        parseFloat(value) > currentCTC ||
-        "Expected CTC should be higher than current CTC"
-      );
+    validate: (value) => {
+      if (value && isNaN(value)) {
+        return "Value must be a valid number.";
+      }
+      return true;
     },
   },
   resumeFile: {
