d01c3b7f356ec4f1f18bba73db558c731d6e16c9
kiranatvav <kiranatvav@gmail.com>
2025-03-05 10:58:27 +0530

    image modal fixes

--- client/src/components/Modals/ImageModal.jsx
+++ client/src/components/Modals/ImageModal.jsx
@@ -15,24 +15,27 @@ function ImageModal({isOpen, onClose, imageUrl}) {
         />
 
         {/* Modal content */}
-        <StyledCard extraStyles="relative w-[80%] max-w-4xl z-10">
+        <StyledCard backgroundColor={"bg-transparent"} extraStyles="relative w-[80%] max-w-4xl z-10 overfflow-hidden">
             {/* Close button */}
-            <button
-                onClick={onClose}
-                className="absolute top-2 right-2 p-2 bg-background-90 hover:bg-background-80 rounded-full transition-colors z-20"
-            >
-                <X className="w-6 h-6 text-font-gray" />
-            </button>
+
 
             {/* Video title */}
             {/* <h2 className="typography-h2 mb-4">Assessment Recording</h2> */}
 
             {/* Video container with loader */}
-            <div className="relative aspect-video w-full bg-background-80 rounded-lg overflow-hidden">
-                <img
-                    src={imageUrl}
-                    className="w-full h-full rounded-lg"
-                />
+            <div className="relative  w-full  rounded-lg ">
+                <div className='h-full w-fit relative mx-auto'>
+                    <button
+                        onClick={onClose}
+                        className="absolute -top-2 -right-2 p-2 bg-background-90 hover:bg-background-80 rounded-full transition-colors z-20"
+                    >
+                        <X className="w-6 h-6 text-font-gray" />
+                    </button>
+                    <img
+                        src={imageUrl}
+                        className="h-full mx-auto rounded-lg"
+                    />
+                </div>
             </div>
         </StyledCard>
     </div>
