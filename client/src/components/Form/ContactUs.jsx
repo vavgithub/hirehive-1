d915df40ca9e7e419915e19f2158d95175f84d87
vavgithub <helloatvavgithub.com>
2025-03-18 18:26:23 +0530

    hover state is fixed

--- client/src/components/Form/ContactUs.jsx
+++ client/src/components/Form/ContactUs.jsx
@@ -335,7 +335,7 @@ const ContactUs = () => {
 
       <button
         onClick={toggleModal}
-        className="fixed bottom-6 right-6 z-40 w-14 h-14 rounded-xl bg-background-70 hover:text-accent-100 text-white shadow-lg flex items-center justify-center transition-all duration-300 hover:scale-105"
+        className="fixed bottom-6 right-6 z-40 w-14 h-14 rounded-xl bg-background-70 hover:bg-accent-300 text-white shadow-lg flex items-center justify-center transition-all duration-300 hover:scale-105"
         aria-label="Contact Us"
         >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
