80ed142471e57d80b1cef2d5ba44941a585c7d49
kiranatvav <kiranatvav@gmail.com>
2025-04-15 19:02:40 +0530

    style fixes for SideCard

--- client/src/components/ui/SideCard.jsx
+++ client/src/components/ui/SideCard.jsx
@@ -2,44 +2,48 @@ import React from 'react'
 import { postedDate } from '../../utility/postedDate';
 import IconWrapper from '../Cards/IconWrapper';
 import { Calendar, FileText, GraduationCap, PenTool } from 'lucide-react';
+import StyledCard from '../Cards/StyledCard';
 
 const SideCard = ({ formData }) => {
     const formattedDate = postedDate(formData.createdAt);
     return (
 
-        <div className="grid grid-cols-2 gap-3 w-full rounded-xl p-6 bg-background-90 md:mt-4 md:flex md:flex-col md:gap-6  " >
-            <div className="flex items-center typography-body ">
-                <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={FileText} />
-                <div className="ml-2">
-                    <p className="large-p">{formData.employmentType}</p>
-                    <p className="text-sm text-font-gray">Employment Type</p>
+        <StyledCard>
+            <h3 className='typography-h3 mr-8 mb-4'>Job Overview</h3>
+            <div className="grid grid-cols-2 gap-3 w-full  md:flex md:flex-col md:gap-6  " >
+                <div className="flex items-center w-full typography-body ">
+                    <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={FileText} />
+                    <div className="ml-2 w-full overflow-hidden">
+                        <p className="typography-body">{formData.employmentType}</p>
+                        <p className="typography-small-p text-font-gray whitespace-nowrap text-ellipsis overflow-hidden w-full">Employment Type</p>
+                    </div>
                 </div>
-            </div>
-            <div className="flex items-center">
-                <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={PenTool} />
-                <div className="ml-2">
-                    <p className="large-p">{formData.jobProfile}</p>
-                    <p className="text-sm text-font-gray">Job Profile</p>
+                <div className="flex items-center w-full">
+                    <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={PenTool} />
+                    <div className="ml-2 w-full overflow-hidden">
+                        <p className="typography-body">{formData.jobProfile}</p>
+                        <p className="typography-small-p text-font-gray whitespace-nowrap text-ellipsis overflow-hidden w-full">Job Profile</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="flex items-center">
-                <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={GraduationCap} />
-                <div className="ml-2">
-                    <p className="large-p">{formData.experienceFrom} - {formData.experienceTo} Year</p>
-                    <p className="text-sm text-font-gray">Experience Level</p>
+                <div className="flex items-center w-full">
+                    <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={GraduationCap} />
+                    <div className="ml-2 w-full overflow-hidden">
+                        <p className="typography-body">{formData.experienceFrom} - {formData.experienceTo} Year</p>
+                        <p className="typography-small-p text-font-gray whitespace-nowrap text-ellipsis overflow-hidden w-full">Experience Level</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="flex items-center">
-                <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={Calendar} />
-                <div className="ml-2">
-                    <p className="large-p ">{formattedDate}</p>
-                    <p className="text-sm text-font-gray">Date Posted </p>
+                <div className="flex items-center w-full">
+                    <IconWrapper size={0} customIconSize={5} customStrokeWidth={1} isInActiveIcon icon={Calendar} />
+                    <div className="ml-2 w-full overflow-hidden">
+                        <p className="typography-body ">{formattedDate}</p>
+                        <p className="typography-small-p text-font-gray whitespace-nowrap text-ellipsis overflow-hidden w-full">Date Posted </p>
+                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+        </StyledCard>
 
     )
 }
