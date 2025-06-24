dd6dc97579a773b0e2c539b6442896c71274787e
kiranatvav <kiranatvav@gmail.com>
2025-06-24 15:56:59 +0530

    cicd backend changed

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Ensure correct PATH
-export PATH="/usr/bin:/usr/local/bin:$PATH"
-
-cd /home/ubuntu
-npm install
--- scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu
-pm2 restart api || pm2 start npm --name hhv-staging -- run staging
-pm2 save
