cdaf84145f67268a3a9f01e45acc708847f1e3c5
kiranatvav <kiranatvav@gmail.com>
2025-06-25 12:48:34 +0530

    CodeDeploy test 12

--- server/scripts/start_server.sh
+++ server/scripts/start_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "[start_server] Starting app with PM2..."
+echo "[start_server] Killing any rogue process on port 8008..."
+sudo kill -9 $(sudo lsof -ti:8008) || true
 
+echo "[start_server] Starting app with PM2..."
 sudo -u ubuntu bash -lc "
   cd /home/ubuntu/hirehive-1/server && \
   pm2 delete hhv-staging || true && \
