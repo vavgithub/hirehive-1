8f6484645ff69c0cf664bf6f8e9e79c8f08e150a
kiranatvav <kiranatvav@gmail.com>
2025-06-25 11:44:04 +0530

    CodeDeploy test 5

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -2,6 +2,3 @@
 
 echo "Restoring .env.staging"
 mv /home/ubuntu/hirehive-1/.env.staging.bak /home/ubuntu/hirehive-1/server/.env.staging || true
-
-echo "Fixing ownership"
-chown -R ubuntu:ubuntu /home/ubuntu/hirehive-1
