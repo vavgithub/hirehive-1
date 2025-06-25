b375a1d36ebbf8d9345505a69ead955023400ca8
kiranatvav <kiranatvav@gmail.com>
2025-06-25 11:39:56 +0530

    CodeDeploy test 4

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+set -e
+
 echo "Backing up existing .env.staging"
+
+# Backup .env.staging (if exists)
 cp /home/ubuntu/hirehive-1/server/.env.staging /home/ubuntu/hirehive-1/.env.staging.bak || true
+
+echo "Fixing ownership of server directory"
+sudo chown -R ubuntu:ubuntu /home/ubuntu/hirehive-1/server
+
+echo "Cleaning old server files"
 rm -rf /home/ubuntu/hirehive-1/server/*
