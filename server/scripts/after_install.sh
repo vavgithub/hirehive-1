8b23d14e152cf59dd4262d5c3bcbcddcc52f8329
kiranatvav <kiranatvav@gmail.com>
2025-06-25 12:26:53 +0530

    CodeDeploy test 11

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -4,6 +4,8 @@ set -e
 echo "[after_install] Restoring .env.staging..."
 mv /home/ubuntu/hirehive-1/.env.staging.bak /home/ubuntu/hirehive-1/server/.env.staging || true
 
+echo "[after_install] Fixing ownership before npm install..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/hirehive-1/server
+
 echo "[after_install] Installing node modules on EC2..."
-cd /home/ubuntu/hirehive-1/server
 sudo -u ubuntu bash -lc "cd /home/ubuntu/hirehive-1/server && npm install"
