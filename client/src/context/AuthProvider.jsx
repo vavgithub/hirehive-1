ae9dc975470458de7367aefabbebcf79c6c52dde
kiranatvav <kiranatvav@gmail.com>
2025-08-05 12:30:28 +0530

    refresh to dashboard issue fixed on ADMIN side

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -9,15 +9,17 @@ const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const { data, isLoading, error } = useAuth();
   const [user,setUser] = useState(null);
+  const [isDone,setIsDone] = useState(false);
 
   useEffect(()=>{
     if(!isLoading){
       setUser(data)
+      setIsDone(true)
     }
   },[user,isLoading])
 
   return (
-    <AuthContext.Provider value={{ user, isLoading, error ,setUser }}>
+    <AuthContext.Provider value={{ user, isLoading, error , isDone, setUser }}>
       {children}
     </AuthContext.Provider>
   );
