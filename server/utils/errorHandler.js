cb496924d74fdddee8e3008ea5855d092dd6bb20
Khyathi Neerukonda <khyathiatvav@gmail.com>
2026-05-19 17:01:10 +0530

    fix: add sentry.js import to index.js and use searchable tags in captureError

--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -2,5 +2,12 @@ import * as Sentry from "@sentry/node";
 
 export const captureError = (error, context = {}) => {
   console.error(error);
-  Sentry.captureException(error, { extra: context });
+  Sentry.captureException(error, {
+    tags: {
+      file: context.file,
+      action: context.action,
+      role: context.role,
+    },
+    extra: context,
+  });
 };
